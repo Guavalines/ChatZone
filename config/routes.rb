16c9874e64930bc20ba68554ba4fc16ed4c66703
guavalines <konstrictor@gmail.com>
2023-01-12 21:46:12 -0800

    created seed file and made root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :rooms
+  root "rooms#index"
+  resources :rooms do
+    resources :messages
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
