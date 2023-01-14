036fc7274cd4a152bf30976a4a90fefdb3d455e2
guavalines <konstrictor@gmail.com>
2023-01-13 23:44:02 -0800

    connected js target to container id

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,5 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_room, only: %i[ show edit update destroy ]
 
   # GET /rooms or /rooms.json
--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -2,8 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="messages"
 export default class extends Controller {
-  static targets = ["container"];
+  static targets = ["container", "input_form"];
   connect() {
-    console.log("connected");
+    console.log(this.containerTarget);
   }
 }
